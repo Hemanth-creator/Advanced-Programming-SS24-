977c6b9d7bcfb32b885d0b2949ca923c2f4df7ae
V_Hemanth_Reddy <hemanth.vudavagandla@gmail.com>
2024-04-06 21:08:49 +0530

    List1 solved Solutions

---
+++ Python exercises - Google/basic/list1.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Basic list exercises
+# Fill in the code for the functions below. main() is already set up
+# to call the functions with a few different inputs,
+# printing 'OK' when each function is correct.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+# It's ok if you do not complete all the functions, and there
+# are some additional functions to try in list2.py.
+
+# A. match_ends
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+def match_ends(words):
+  count=0 # intiliaze the count variable and assign it to zero
+  for word in words: #iterate through all the words from the list.
+    if len(word) >= 2 and word[0] == word[-1]: # check if the length of the words is grater than or equal to 2 and first letter and last letter is same
+      count+=1 # increment the count by 1
+
+  return count
+
+
+# B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
+def front_x(words):
+  X_words=[] # initiate a empty list which stores words starting with x
+  other_words=[] # initiate a empty list that stores words which do not start with x
+  for word in words: # iterate the words
+    if(word[0]=="x"): # check if it starts with x
+      X_words.append(word) # add the word that starts with x
+    else:
+      other_words.append(word) # add the other words
+  X_words.sort() #sort the list in alphabetical order
+  other_words.sort()
+  return X_words+other_words
+
+
+
+# C. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+def sort_last(tuples):
+  n = len(tuples) #calculate the length
+  for i in range(n):
+    for j in range(0, n-i-1):
+      if tuples[j][-1] > tuples[j+1][-1]: #Check if the last element of the current tuple is greater than the last element of the next tuple
+        tuples[j], tuples[j+1] = tuples[j+1], tuples[j] # swap the tuples
+  return tuples
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+  print('match_ends')
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+  print()
+  print('front_x')
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+
+  print()
+  print('sort_last')
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+  main()
